63aef8d2582ccb1c9203610d9e71bbcaf2f3c5f3
fumihiro.sunada <fumihiro.sunada@bigtreetc.com>
2024-04-30 18:22:23 +0900

    ログ表示の時系列が気持ち悪いのでSleepを挟んでおく

--- function_app.py
+++ function_app.py
@@ -1,4 +1,5 @@
 import logging
+import time
 import azure.functions as func
 
 app = func.FunctionApp()
@@ -7,10 +8,14 @@ app = func.FunctionApp()
               use_monitor=False) 
 def timer_trigger_vscode(myTimer: func.TimerRequest) -> None:
     logging.info('---[START]---')
+    time.sleep(0.1)
     logging.info('!!!!! Hello, world !!!!!')
+    time.sleep(0.1)
 
     if myTimer.past_due:
         logging.info('The timer is past due!')
+        time.sleep(0.1)
 
     logging.info('Python timer trigger function executed.')
+    time.sleep(0.1)
     logging.info('---[END]---')
